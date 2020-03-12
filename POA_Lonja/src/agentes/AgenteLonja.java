258687bc4f911d0643f84f8c4865422fb51059a2
Eduardo <edusalcas@hotmail.com>
2020-03-12 17:58:38 +0100

    Primera aproximación AgenteLonja y (protocolo-registro-vendedor)
    Done:
     - Implementar clase AgenteLonja para que se cree una lonja.
     - Implementar (protocolo-registro-vendedor) entre RV y RAV
    
    Todo:
      - Implementar clase Agente comprador para que se cree un comprador. (Hita)
     - Establecer comportamientos de los roles para los distintos protocolos
     - Implementar (protocolo-deposito)
     - Distinción entre roles (?)

--- POA_Lonja/src/agentes/AgenteLonja.java
+++ POA_Lonja/src/agentes/AgenteLonja.java
@@ -1,7 +1,92 @@
 package agentes;
 
-import jade.core.Agent;
+import java.util.ArrayList;
 
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+@SuppressWarnings("serial")
 public class AgenteLonja extends Agent {
+	// The list of know seller agents
+	private ArrayList<AID> sellerAgents;
+
+	// Put agent initializations here
+	@Override
+	protected void setup() {
+		System.out.println("Soy el agente lonja " + this.getName());
+
+		sellerAgents = new ArrayList<AID>();
+
+		// Register the book-selling service in the yellow pages
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("lonja");
+		sd.setName("JADE-Lonja");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		// (protocolo-registro-vendedor) El RAV recibe la petición de registro del RV
+		addBehaviour(new RequestRegistroVendedor());
+	}
+
+	@Override
+	protected void takeDown() {
+		// Deregister from the yellow pages
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		// Printout a dismissal message
+		System.out.println("Lonja-agent " + getAID().getName() + " terminating.");
+
+		super.takeDown();
+	}
+
+	/*
+	 * Clase privada que se encarga de recibir los mensajes tipo request del vendedor
+	 * para registrarse en la lonja. En caso de que el vendedor aun no este registrado,
+	 * se le registrara. De esta comunicación se encarga el RAV.
+	 */
+	private class RequestRegistroVendedor extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+			ACLMessage msg = myAgent.receive(mt);
+			AID sender;
+			if (msg != null) {
+				// Request Message received. Process it.
+				ACLMessage reply = msg.createReply();
+				sender = msg.getSender();
+
+				if (!sellerAgents.contains(sender)) {
+					// Seller can be registered
+					sellerAgents.add(sender);
+					reply.setPerformative(ACLMessage.INFORM);
+				} else {
+					// Seller cant be registered
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("Fallo en el registro");
+				}
+
+				myAgent.send(reply);
+			} else {
+				block();
+			}
+		}
+	}
+	// End of inner class RequestRegistroVendedor
 
 }
--- POA_Lonja/src/agentes/AgenteVendedor.java
+++ POA_Lonja/src/agentes/AgenteVendedor.java
@@ -1,19 +1,24 @@
 package agentes;
 
-import java.util.Hashtable;
-
 import gui.GuiComprador;
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 @SuppressWarnings("serial")
 public class AgenteVendedor extends Agent {
 
 	// The GUI by means of which the user can add books in the catalogue
 	private GuiComprador myGui;
+
+	private AID lonjaAgent = new AID("lonja", AID.ISLOCALNAME);
 
 	// Put agent initializations here
 	@Override
@@ -36,6 +41,8 @@ public class AgenteVendedor extends Agent {
 		} catch (FIPAException fe) {
 			fe.printStackTrace();
 		}
+
+		addBehaviour(new RequestRegistro());
 	}
 
 	// Put agent clean-up operations here
@@ -49,10 +56,65 @@ public class AgenteVendedor extends Agent {
 
 		// Printout a dismissal message
 		System.out.println("Seller-agent " + getAID().getName() + " terminating.");
+		super.takeDown();
 	}
 
 	public void nuevaMercancia(String title, int cantidad) {
 		System.out.println("Nuevo paquete de mercancia con " + cantidad + "kg de " + title);
-		
 	}
+
+	/*
+	 * Clase privada que se encarga del registro del vendedor, le manda un mensaje tipo
+	 * request y el RAV le responde si se le ha registrado correctamente o no
+	 */
+	private class RequestRegistro extends Behaviour {
+		private MessageTemplate mt; // The template to receive replies
+		private int step = 0;
+
+		@Override
+		public void action() {
+			switch (step) {
+			case 0:
+				// Enviar request al agente lonja con rol RAV
+				ACLMessage req = new ACLMessage(ACLMessage.REQUEST);
+
+				req.addReceiver(lonjaAgent);
+				req.setConversationId("registro-vendedor");
+				req.setReplyWith("req" + System.currentTimeMillis()); // Unique value
+
+				myAgent.send(req);
+				// Prepare the template to get proposals
+				mt = MessageTemplate.and(MessageTemplate.MatchConversationId("registro-vendedor"),
+						MessageTemplate.MatchInReplyTo(req.getReplyWith()));
+				step = 1;
+				break;
+			case 1:
+				// Receive all proposals/refusals from seller agents
+				ACLMessage reply = myAgent.receive(mt);
+				if (reply != null) {
+					// Reply received
+					if (reply.getPerformative() == ACLMessage.INFORM) {
+						// Registro exitoso
+						System.out.println("Registrado con exito");
+						step = 2;
+					} else {
+						// Fallo en el registro
+						System.out.println(reply.getContent());
+					}
+
+				} else {
+					block();
+				}
+				break;
+			}
+
+		}
+
+		@Override
+		public boolean done() {
+			return step == 2;
+		}
+
+	}
+	// End of inner class RequestRegistro
 }
