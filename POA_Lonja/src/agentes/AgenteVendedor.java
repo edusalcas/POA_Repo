e98e98643b6514f49ac3cec1f494833a5780aadb
Eduardo <edusalcas@hotmail.com>
2020-03-12 14:31:48 +0100

    Primera aproximación AgenteVendedor y GuiVendedor
    Done:
     - Creada la clase GuiVendedor dentro del paquete gui para la interfaz del vendedor.
     - Añadida funcionalidad para crearse y recibir nuevos paquetes en el vendedor.
    
    Todo:
      - Implementar clase Agente comprador para que se cree un comprador. (Hita)
     - Establecer comportamientos de los roles para los distintos protocolos
     - Implementar clase AgenteLonja para que se cree una lonja.
     - Implementar Rol encargado de Admisión de Vendedores (RAV) en la lonja.
     - Implementar (protocolo-registro-vendedor) entre RV y RAV

--- POA_Lonja/src/agentes/AgenteVendedor.java
+++ POA_Lonja/src/agentes/AgenteVendedor.java
@@ -1,7 +1,58 @@
 package agentes;
 
+import java.util.Hashtable;
+
+import gui.GuiComprador;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
-public class AgenteVendedor extends Agent{
+@SuppressWarnings("serial")
+public class AgenteVendedor extends Agent {
 
+	// The GUI by means of which the user can add books in the catalogue
+	private GuiComprador myGui;
+
+	// Put agent initializations here
+	@Override
+	protected void setup() {
+		System.out.println("Soy el agente vendedor " + this.getName());
+
+		// Create and show the GUI
+		myGui = new GuiComprador(this);
+		myGui.showGui();
+
+		// Register the book-selling service in the yellow pages
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("vendedor");
+		sd.setName("JADE-Lonja");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+	}
+
+	// Put agent clean-up operations here
+	protected void takeDown() {
+		// Deregister from the yellow pages
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		// Printout a dismissal message
+		System.out.println("Seller-agent " + getAID().getName() + " terminating.");
+	}
+
+	public void nuevaMercancia(String title, int cantidad) {
+		System.out.println("Nuevo paquete de mercancia con " + cantidad + "kg de " + title);
+		
+	}
 }
